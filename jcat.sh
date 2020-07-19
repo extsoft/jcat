ae7363ec9c1b0a4cf77d3e02a65e3326362a2cfa
Dmytro Serdiuk <dmytro.serdiuk@gmail.com>
2020-07-19 22:27:13 +0300

    Wait for Selenium daemon is ready
    Sometimes happens that a Selenium daemon (means here webdriver binary
    or Selenium Grid) is not ready to work. In this case, the tests should
    wait a bit before actually start testing. That's why the waiting logic
    is added to verify if the daemon is ready or not for 30 seconds. If
    it is not ready, the tests won't start and the execution will fail with
    exit code 0.

--- jcat.sh
+++ jcat.sh
@@ -1,6 +1,41 @@
 #!/bin/sh
 set -e
 
-COMMAND=$(echo "java -Dselenium-url=${SELENIUM_URL} -Dbrowser=${BROWSER} -jar jcat.jar")
-echo "Execute: ${COMMAND}"
-eval "${COMMAND}"
+highlight() {
+  echo " >>> ${*}"
+}
+
+wait_selenium() {
+  highlight "Waiting for Selenium daemon is ready to accept connections..."
+  counter=0
+  while true; do
+    counter=$((counter+1))
+    ( wget --quiet --output-document - ${SELENIUM_URL}/status | grep '"ready":true') ||
+      {
+        if test ${counter} -gt 30 ; then
+          echo "Operation timed out!" >&2
+          echo "Selenium daemon on is not ready after 30 seconds." >&2
+          echo "Selenium daemon URL: ${SELENIUM_URL}" >&2
+          exit 1
+        fi
+        echo 'Selenium daemon is not ready... Waiting one more second...'
+        sleep 1s
+        continue
+      }
+    highlight 'Selenium daemon is ready!'
+    break
+  done
+}
+
+run_jcat() {
+  COMMAND="java -Dselenium-url=${SELENIUM_URL} -Dbrowser=${BROWSER} -jar jcat.jar"
+  highlight "Run tests: ${COMMAND}"
+  exec ${COMMAND}
+}
+
+main() {
+  wait_selenium
+  run_jcat
+}
+
+main
